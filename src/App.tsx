1ca7aaf74884ca136cfb83acfae7bee46e97d249
lsy0xcc <lsy0xcc@outlook.com>
2022-10-17 21:24:02 +0800

    Add less

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './index.less';
 import Route from './route';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
