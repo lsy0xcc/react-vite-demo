26c03a526aa3d54d1e7fadfe9e316f6cb1854d13
lsy0xcc <lsy0xcc@outlook.com>
2022-11-13 22:32:21 +0800

    Update axios

--- src/services/simpleReq.ts
+++ src/services/simpleReq.ts
@@ -1,7 +1,29 @@
 import axios from './axios';
 
 export default {
-  getHello: async () => axios.get('/long-time-hello'),
+  getHello: async () => {
+    try {
+      const res = await axios.get('/long-time-hello');
+      return res;
+    } catch (err: any) {
+      if (err.response) {
+        // The request was made and the server responded with a status code
+        // that falls out of the range of 2xx
+        console.log(err.response.data);
+        console.log(err.response.status);
+        console.log(err.response.headers);
+      } else if (err.request) {
+        // The request was made but no response was received
+        // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+        // http.ClientRequest in node.js
+        console.log(err.request);
+      } else {
+        // Something happened in setting up the request that triggered an Error
+        console.log('Error', err.message);
+      }
+      throw err;
+    }
+  },
   getHelloUser: async (input = '') =>
     axios.get('/hello-user', { params: { name: input } }),
 };
